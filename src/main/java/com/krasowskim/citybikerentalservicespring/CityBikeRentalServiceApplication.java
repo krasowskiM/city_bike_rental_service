da13665d34bbed3f0e5a6c066a13d99b6a3e1c3a
krasowskiM <krasowski.m87@gmail.com>
2023-02-01 18:44:21 +0100

    Initial commit, started api definition

---
+++ src/main/java/com/krasowskim/citybikerentalservicespring/CityBikeRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krasowskim.citybikerentalservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBikeRentalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityBikeRentalServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasowskim/citybikerentalservicespring/CityBikeRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasowskim.citybikerentalservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBikeRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
